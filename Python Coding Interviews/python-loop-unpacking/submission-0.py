55495912b0cede488558d84c7151d03d6cb1e1d9
Gowtham Shankar <gowthamshankar0914@gmail.com>
2026-06-18 10:10:53 -0400

    Add: python-loop-unpacking - submission-0

---
+++ Python Coding Interviews/python-loop-unpacking/submission-0.py
@@ -0,0 +1,22 @@
+from typing import List, Tuple
+
+
+def best_student(scores: List[Tuple[str, int]]) -> str:
+    max = 0
+    max_student = ""
+    for score in scores:
+        name, ind_score = score
+        if ind_score > max:
+            max = ind_score
+            max_student = name
+    
+    return max_student
+
+
+
+
+# do not modify below this line
+print(best_student([("Alice", 90), ("Bob", 80), ("Charlie", 70)]))
+print(best_student([("Alice", 90), ("Bob", 80), ("Charlie", 100)]))
+print(best_student([("Alice", 90), ("Bob", 100), ("Charlie", 70)]))
+print(best_student([("Alice", 90), ("Bob", 90), ("Charlie", 80), ("David", 100)]))
