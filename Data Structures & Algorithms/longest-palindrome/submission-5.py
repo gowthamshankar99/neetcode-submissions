960676bb9cdfee6c1826e3abed4defaa552d57f6
Gowtham Shankar <gowthamshankar0914@gmail.com>
2026-07-21 08:46:22 -0400

    Add: longest-palindrome - submission-5

---
+++ Data Structures & Algorithms/longest-palindrome/submission-5.py
@@ -0,0 +1,34 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+
+        map = {}
+
+        for single in s:
+            if single in map:
+                map[single] += 1
+            else:
+                map[single] = 1
+        
+        print(map)
+
+        # loop through the map 
+        res = 0 
+        is_one = False
+        
+        # loop through the map first and when you find a odd number 
+        #- add the 1 to it 
+        # and flip the flag 
+
+        for key,value in map.items():
+
+            if value % 2 == 0:
+                res = res + value 
+            elif value > 1:
+                res += value - 1
+                is_one = True
+            else:
+                is_one = True
+
+        
+        return res if not is_one else res+1
+        
