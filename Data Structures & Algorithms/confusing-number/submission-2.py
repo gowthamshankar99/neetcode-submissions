adb5cda6af7c7ee415badf96c64124fd0497d391
Gowtham Shankar <gowthamshankar0914@gmail.com>
2026-07-26 17:26:50 -0400

    Add: confusing-number - submission-2

---
+++ Data Structures & Algorithms/confusing-number/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def confusingNumber(self, n: int) -> bool:
+        map = {'1': '1', '2': '2', '6': '9', '8': '8', '9': '6', '0': '0'}
+        res = ""
+        for num in str(n):
+            if num not in map:
+                return False
+            else:
+                res += map[num]
+
+        print(res, "".join(reversed(str(n))))
+        # valid number at this point
+        if res == "".join(reversed(str(n))):
+            return False
+        
+        return True
+        
